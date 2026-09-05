a45a833e515858aae5ed77413640388d5f159bd3
HttpAnimations <shawndouglasyt@gmail.com>
2026-09-05 13:07:46 -0700

    fix: 修复消息结束后输入框在移动端自动聚焦的问题

--- flutter/lib/views/thread_page.dart
+++ flutter/lib/views/thread_page.dart
@@ -1,5 +1,6 @@
 import 'dart:math' as math;
 
+import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/rendering.dart';
 import 'package:flutter/services.dart';
